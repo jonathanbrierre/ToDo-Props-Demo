9c81823e4dd88e060c0efb085ba63f6aeb1cb484
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-06-06 17:19:44 -0400

    cleaned up files & created component tree

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    todos: [
+      {
+        task: 'Mow Lawn',
+        completed: false
+      },
+      {
+        task: 'Cook Dinner',
+        completed: false
+      },
+      {
+        task: 'Create a Todo List app',
+        completed: true
+      }
+    ]
+  }
+
+  render(){
+    return (
+      <div className="App">
+        Your Super Fancy To Do List
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/TodosContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Todo from './Todo'
+
+class TodosContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TodosContainer
